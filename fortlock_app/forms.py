5caa743172ec7bac22a460eacbe26bf7d690d916
AdrielCavalcante <adriel.cavalcante2004@gmail.com>
2023-10-30 21:38:24 -0300

    :sparkles: Feat: Formulário de Usuário
    Foi criada a tratativa inicial de formulário da tabela usuário, lá em forms.py, há a validação de e-mail e de senhas, além do uso de HASH SHA-256 aplicado na senha, dentro de views.py. A transaction é para garantir a atomicidade, caso algo de errado, o SQL não irá rodar pela metade

--- fortlock_app/forms.py
+++ fortlock_app/forms.py
@@ -1,5 +1,36 @@
+import re
 from django import forms
+from django.core.validators import validate_email
+from django.core.exceptions import ValidationError
 
+class UsuarioForm(forms.Form):
+    matricula = forms.CharField(label='Sua matrícula', max_length=30, required=True)
+    nome = forms.CharField(label='Seu nome', max_length=50, required=True)
+    email = forms.EmailField(label='Seu e-mail', required=True)
+    senha = forms.CharField(label='Seu senha', max_length=30, required=True)
 
-class NomeForm(forms.Form):
-    nome = forms.CharField(label='Seu nome', max_length=100)
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if email:
+            try:
+                validate_email(email)
+            except ValidationError as e:
+                self.add_error('email', "Endereço de e-mail inválido.\n"+e)
+        return email
+
+    def clean_senha(self):
+        senha = self.cleaned_data.get('senha')
+        if senha:
+            if len(senha) < 8:
+                raise ValidationError("A senha deve ter no mínimo 8 caracteres.")
+
+            if not any(char.isupper() for char in senha):
+                raise ValidationError("A senha deve conter pelo menos 1 letra maiúscula.")
+
+            if not any(char.islower() for char in senha):
+                raise ValidationError("A senha deve conter pelo menos 1 letra minúscula.")
+
+            if not re.search(r'[!@#\$%\^&\*\(\)_\+\-=\[\]{};:\'",<>\./?\\|]$', senha):
+                raise ValidationError("A senha deve conter pelo menos 1 caractere especial (!@#$%^&*()_+-=[]{};:'\",<>/?.|\\).")
+
+        return senha
--- fortlock_app/views.py
+++ fortlock_app/views.py
@@ -1,20 +1,34 @@
-import requests
+# REQUISIÇÃO API import requests
 from django.shortcuts import render, redirect
 from django.http import HttpResponse, JsonResponse
+from django.db import transaction 
 from hashlib import sha256
-from .forms import NomeForm
+from .forms import UsuarioForm
 from .models import Usuario
 
 
 def pagina_inicial(request):
     if request.method == 'POST':
-        form = NomeForm(request.POST)
+        form = UsuarioForm(request.POST)
         if form.is_valid():
+            matricula = form.cleaned_data['matricula']
             nome = form.cleaned_data['nome']
-            Usuario.objects.create(nome=nome, matricula='MATRICULAEXEMPLO', email='email@example.com', senha='senhaDIFICIL')
+            email = form.cleaned_data['email']
+            senha = form.cleaned_data['senha']
+
+            cifra = sha256()
+            cifra.update(senha.encode('utf-8'))
+            senhaCifrada = cifra.hexdigest()
+
+            try:
+                with transaction.atomic():
+                    Usuario.objects.create(matricula=matricula, nome=nome, email=email, senha=senhaCifrada)
+            except Exception as e:
+                print("Deu ruim: "+e)
+
             return redirect('mostrar_nome')
     else:
-        form = NomeForm()
+        form = UsuarioForm()
     return render(request, 'teste_app/pagina_inicial.html', {'form': form})
 
 
